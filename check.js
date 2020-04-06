b35af976a25db851bf777d90cfdb62ba00c7f2b0
edisonhello <edisonhello@hotmail.com>
2020-04-06 21:34:22 +0800

    Add checker

--- check.js
+++ check.js
@@ -1,3 +1,4 @@
+const PriorityQueue = () => {const top=0,parent=c=>(c+1>>>1)-1,left=c=>(c<<1)+1,right=c=>c+1<<1;class PriorityQueue{constructor(c=(d,e)=>d>e){this._heap=[],this._comparator=c}size(){return this._heap.length}isEmpty(){return 0==this.size()}peek(){return this._heap[top]}push(...c){return c.forEach(d=>{this._heap.push(d),this._siftUp()}),this.size()}pop(){const c=this.peek(),d=this.size()-1;return d>top&&this._swap(top,d),this._heap.pop(),this._siftDown(),c}replace(c){const d=this.peek();return this._heap[top]=c,this._siftDown(),d}_greater(c,d){return this._comparator(this._heap[c],this._heap[d])}_swap(c,d){[this._heap[c],this._heap[d]]=[this._heap[d],this._heap[c]]}_siftUp(){for(let c=this.size()-1;c>top&&this._greater(c,parent(c));)this._swap(c,parent(c)),c=parent(c)}_siftDown(){for(let d,c=top;left(c)<this.size()&&this._greater(left(c),c)||right(c)<this.size()&&this._greater(right(c),c);)d=right(c)<this.size()&&this._greater(right(c),left(c))?right(c):left(c),this._swap(c,d),c=d}}return PriorityQueue}
 
 const fs = require('fs')
 const path = require('path')
@@ -6,10 +7,14 @@ const test_folder = path.join(__dirname, 'testset')
 const output_folder = path.join(__dirname, 'output')
 
 async function ReadInput(fname) {
-	return fs.readFileSync(fname, 'utf-8').split('\r\n').splice(2).map(d => {
-		let j = d.split(' ')
-		return { name: j[0], start: parseInt(j[1]), duration: parseInt(j[2]) }
-	})
+	let i = fs.readFileSync(fname, 'utf-8').replace(/\r\n$/gm, '').split('\r\n')
+	return {
+		type: i[0],
+		jobs: i.splice(2).map(d => {
+			let j = d.split(' ')
+			return { name: j[0], start: parseInt(j[1]), duration: parseInt(j[2]) }
+		})
+	}
 }
 
 async function GetNamePidMap(fname) {
@@ -22,10 +27,15 @@ async function GetNamePidMap(fname) {
 	return j
 }
 
-async function GetResult(fname) {
+async function GetResult(fname, inp, TIME_UNIT) {
 	let r = fs.readFileSync(fname, 'utf-8').split('\n').slice(0, -1).map(i => i.split(' ').splice(-3, 3).map(x => parseFloat(x)))
 	let mintime = 1e20
 	for (const k of r) mintime = Math.min(mintime, k[1], k[2])
+	if (inp !== undefined) {
+		let mintick = 1e20
+		for (const k of inp.jobs) mintick = Math.min(mintick, k.start)
+		mintime -= mintick * TIME_UNIT
+	}
 	let u = {}
 	for (let k of r) u[k[0]] = { start: k[1] - mintime, end: k[2] - mintime }
 	return u
@@ -35,34 +45,148 @@ async function CalcTimeUnit() {
 	let inp = await ReadInput(path.join(test_folder, 'TIME_MEASUREMENT.txt'))
 	let mp = await GetNamePidMap(path.join(output_folder, 'TIME_MEASUREMENT_stdout.txt'))
 	let exet = await GetResult(path.join(output_folder, 'TIME_MEASUREMENT_dmesg.txt'))
-	return exet[mp['P9']].end / (inp[9].start + inp[9].duration)
+	return exet[mp['P9']].end / (inp.jobs[9].start + inp.jobs[9].duration)
 }
 
+async function Simulation(td) {
+	let njob = td.jobs.reduce((_, v, i, a) => { 
+		_.push({ i, v })
+		return _
+	}, []).sort((a, b) => (a.v.start == b.v.start ? a.i - b.i : a.v.start - b.v.start))
 
-;(async () => {
+	let rt = {}
 
-const TIME_UNIT = await CalcTimeUnit()
-console.log(`Realtime unit = ${TIME_UNIT} seconds`)
+	;({ FIFO: td => { 
+		for (let i = 0, nt = 0; i < td.length; i++) {
+			if (td[i].start > nt) nt = td[i].start
+			rt[td[i].name] = { start: nt, end: nt + td[i].duration }
+			nt += td[i].duration
+		}
+	}, RR: td => {
+		let q = []
+		let nt = 0, rrc = 500, it = 0
+		while (1) {
+			if (it == td.length && q.length == 0) break
 
-const tests = await fs.readdirSync(test_folder)
+			if (q.length && q[0].duration == 0) {
+				rrc = 500
+				rt[q[0].name] = { start: q[0].start, end: nt }
+				q.splice(0, 1)
+			}
 
-for (let input of tests) {
-	let test = input.replace(/\.[^/.]+$/, "")
+			if (rrc == 0) {
+				let z = q.splice(0, 1)
+				q.push(z[0])
+				rrc = 500
+			}
 
-	if (test === 'TIME_MEASUREMENT') continue
+			while (it < td.length && td[it].start <= nt) {
+				q.push({ name: td[it].name, start: -1, duration: td[it].duration })
+				it++
+			}
 
-	let output = `${test}_stdout.txt`
-	let kern = `${test}_dmesg.txt`
+			if (q.length) {
+				q[0].duration--
+				rrc--
+				if (q[0].start == -1) q[0].start = nt
+			}
 
-	console.log(`Judging test ${test}`)
+			nt++
+		}
+	}, SJF: td => {
+		let pq = new (PriorityQueue())((a, b) => { return a.duration < b.duration })
+		let job = undefined
+		let nt = 0, it = 0
+		while (1) {
+			if (pq.isEmpty() && job === undefined && it == td.length) break
 
-	let inp = await ReadInput(path.join(test_folder, input))
-	let mp = await GetNamePidMap(path.join(output_folder, output))
-	let exet = await GetResult(path.join(output_folder, kern))
+			if (job && job.duration == 0) {
+				rt[job.name] = { start: job.start, end: nt }
+				job = undefined
+			}
 
-	console.log('inp', inp)
-	console.log('mp', mp)
-	console.log('exet', exet)
+			while (it < td.length && td[it].start <= nt) {
+				pq.push(td[it])
+				it++
+			}
+
+			if (job === undefined && pq.size()) {
+				job = pq.pop()
+				job.start = nt
+			}
+
+			++nt
+			if (job !== undefined) {
+				job.duration--
+			}
+		}
+	}, PSJF: td => {
+		let pq = new (PriorityQueue())((a, b) => { return a.duration < b.duration })
+		let job = undefined
+		let nt = 0, it = 0
+		while (1) {
+			if (pq.isEmpty() && job === undefined && it == td.length) break
+
+			if (job && job.duration == 0) {
+				rt[job.name] = { start: job.start, end: nt }
+				job = undefined
+			}
+
+			while (it < td.length && td[it].start <= nt) {
+				pq.push({ name: td[it].name, start: -1, duration: td[it].duration })
+				if (job !== undefined && job.duration > td[it].duration) {
+					pq.push(job)
+					job = undefined
+				}
+				it++
+			}
+
+			if (job === undefined && pq.size()) {
+				job = pq.pop()
+				if (job.start == -1) job.start = nt
+			}
+
+			++nt
+			if (job !== undefined) {
+				job.duration--
+			}
+		}
+	} }[td.type](JSON.parse(JSON.stringify(td.jobs))))
+
+	return rt
 }
 
-}) ()
+async function main() {
+	const TIME_UNIT = await CalcTimeUnit()
+	console.log(`Realtime unit = ${TIME_UNIT} seconds`)
+
+	const tests = await fs.readdirSync(test_folder)
+
+	for (let input of tests) {
+		let test = input.replace(/\.[^/.]+$/, "")
+
+		if (test === 'TIME_MEASUREMENT') continue
+
+		let output = `${test}_stdout.txt`
+		let kern = `${test}_dmesg.txt`
+
+		console.log(`Judging test ${test}`)
+
+		let inp = await ReadInput(path.join(test_folder, input))
+		let mp = await GetNamePidMap(path.join(output_folder, output))
+		let exet = await GetResult(path.join(output_folder, kern), inp, TIME_UNIT)
+
+		let ther = await Simulation(inp)
+		ther = Object.keys(ther).reduce((_, v, i, a) => {
+			_[mp[v]] = { start: ther[v].start * TIME_UNIT, end: (ther[v].end - 1) * TIME_UNIT }
+			return _
+		}, {})
+
+		console.log('inp', inp)
+		// console.log('mp', mp)
+		console.log('exet', exet)
+		console.log('ther', ther)
+	}
+}
+
+main()
