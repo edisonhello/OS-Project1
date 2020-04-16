efe50edfd499f9bc62b66eb920748d2d16424282
edisonhello <edisonhello@hotmail.com>
2020-04-16 15:24:05 +0800

    Add demo script

---
+++ demo/demo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+	echo "sudo this."
+	exit 1
+fi
+
+in_folder="../testset"
+
+dmesg -C
+
+for i in TIME_MEASUREMENT FIFO_1 PSJF_2 RR_3 SJF_4;
+do
+	echo Demoing $i ...
+	./scheduler < $in_folder/$i.txt
+	dmesg -c | grep Project1
+done
